5dfcc6acc12609ffea8c2eadfa83056435e776ea
Janos Laszlo Vasik <jvasik@redhat.com>
2024-07-18 14:37:59 +0200

    feat: RHINENG-11478 enhance promotion testing on stage with image comparison

--- stage_test.sh
+++ stage_test.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# New Floorist image used by the operator
+NEW_FLOORIST_IMG="$(
+    oc get deployment.apps -o jsonpath='{.items[?(.metadata.name=="floorist-operator-controller-manager")].spec.template.spec.containers[?(.name=="manager")].env[?(.name=="FLOORIST_IMAGE")].value}{"\n"}'
+):$(
+    oc get deployment.apps -o jsonpath='{.items[?(.metadata.name=="floorist-operator-controller-manager")].spec.template.spec.containers[?(.name=="manager")].env[?(.name=="FLOORIST_IMAGE_TAG")].value}{"\n"}'
+)"
+
 CRONJOBS=$(oc get cronjobs | awk 'NR>1 {print $1}')
 
 if [[ -z "$CRONJOBS" ]]; then
@@ -17,6 +24,12 @@ for CRONJOB in $CRONJOBS; do
 
     if [[ "$SUCCESS" != "1" ]]; then
         echo "ERROR: cronjob $CRONJOB has not created successful jobs"
+        exit 1
+    fi
+
+    # Comparing operator's image with the image used by the (cron)jobs
+    if [[ "$NEW_FLOORIST_IMG" != "$(oc get job -l "pod=${CRONJOB}" -o jsonpath='{.items[].spec.template.spec.containers[].image}{"\n"}')" ]]; then
+        echo "ERROR: cronjob $CRONJOB is not configured with the newest Floorist image"
         exit 1
     fi
 done
